78834164db6175acd13131d48f4bf35d6724e757
Rajdeep Saha <saha.rajdeep3@gmail.com>
2020-04-14 00:27:54 +0530

    added checkout item

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,6 +1,6 @@
-import {UserActionTypes} from './user.types';
+import { UserActionTypes } from './user.types';
 
-export const setCuurentUser = user => ({
-    type: UserActionTypes.SET_CUURENT_USER,
-    payload: user
+export const setCurrentUser = user => ({
+  type: UserActionTypes.SET_CURRENT_USER,
+  payload: user
 });
